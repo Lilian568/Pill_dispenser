2a4ce6283c29251a00cdf985ed8def1ecdcf788b
Tomitul <142896603+Tomitul@users.noreply.github.com>
2024-11-28 12:11:06 +0200

    Update main.c

--- main.c
+++ main.c
@@ -1,1 +1,27 @@
-MOI
+#include "led.h"
+#include "button.h"
+#include <stdio.h>
+#include "pico/stdlib.h"
+
+
+bool isButtonPressed() {
+    return gpio_get(SW_0) == 0;
+}
+int main() {
+    stdio_init_all();
+    ledsInit();
+    pwmInit();
+    buttonsInit();
+
+    while (true) {
+        blink();
+
+        if (isButtonPressed()) {
+            allLedsOff();
+            break;
+        }
+    }
+
+    return 0;
+}
+//yes okay
