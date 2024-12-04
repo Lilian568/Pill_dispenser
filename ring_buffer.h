df5aa43b4452b146ff5d149a952c40214d9d7071
Lilian <142903258+Lilian568@users.noreply.github.com>
2024-12-04 11:31:47 +0200

    Update ring_buffer.h

--- ring_buffer.h
+++ ring_buffer.h
@@ -1,3 +1,5 @@
+//defines a ring buffer implementation. provides a circular data structure for storing and processing data
+
 #ifndef UART_IRQ_RING_BUFFER_H
 #define UART_IRQ_RING_BUFFER_H
 
@@ -5,19 +7,19 @@
 #include <stdbool.h>
 
 typedef struct  {
-    int head;
+    int head; //manage the start and end positions of data in the buffer.
     int tail;
     int size;
     uint8_t *buffer;
 } ring_buffer;
 
-void rb_init(ring_buffer *rb, uint8_t *buffer, int size);
-bool rb_empty(ring_buffer *rb);
-bool rb_full(ring_buffer *rb);
-bool rb_put(ring_buffer *rb, uint8_t data);
-uint8_t rb_get(ring_buffer *rb);
+void rb_init(ring_buffer *rb, uint8_t *buffer, int size); //Initializes the ring buffer with a given buffer and size.
+bool rb_empty(ring_buffer *rb); //Checks if the buffer is empty
+bool rb_full(ring_buffer *rb); //Checks if the buffer is full
+bool rb_put(ring_buffer *rb, uint8_t data); // Adds data to the buffer (if not full).
+uint8_t rb_get(ring_buffer *rb); // Retrieves data from the buffer (if not empty).
 
-void rb_alloc(ring_buffer *rb, int size);
-void rb_free(ring_buffer *rb);
+void rb_alloc(ring_buffer *rb, int size); //Dynamically allocates memory for the buffer.
+void rb_free(ring_buffer *rb); //Frees the allocated memory for the buffer.
 
 #endif //UART_IRQ_RING_BUFFER_H
