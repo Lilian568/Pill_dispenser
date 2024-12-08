d7ea143e2f10d8c63669c3f294aeda4a2053a0e7
villiamku <142919825+villiamku@users.noreply.github.com>
2024-12-08 04:39:34 +0200

    Update motor.h

--- motor.h
+++ motor.h
@@ -6,6 +6,9 @@
 // External variable representing the number of steps per revolution
 extern int steps_per_revolution;
 
+// External variable representing the number of steps for one pill dispense
+extern int steps_per_drop;
+
 // Flag indicating whether the motor has been calibrated
 extern bool is_calibrated;
 
@@ -15,8 +18,8 @@ void setup();
 // Set the motor steps to a specific pattern
 void setSteps(int step);
 
-// Rotate the motor by a given number of steps
-void rotate(int step_count);
+// Rotate the motor by a given number of steps with an option to update EEPROM
+void rotate(int step_count, bool update_eeprom);
 
 // Fine-tune the motor's position for precise alignment
 void fine_tune_position();
@@ -27,5 +30,22 @@ void calibrate();
 // Rotate the motor by the calibrated number of steps for one pill dispense
 void rotate_steps_512();
 
+// Test EEPROM connection
+void test_eeprom_connection();
+
+// Check and realign motor position if necessary
+void realign_after_interruption();
+
+void reset_and_realign();
+
+
+// motor.h
+void maybe_update_eeprom_periodically(void);
+
+// Getter for the current motor step
+int get_current_motor_step();
+
+// Setter for the current motor step
+void set_current_motor_step(int step);
 
 #endif // MOTOR_H
